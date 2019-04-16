fbf0747b9dc8e91fcb94ab3ce2ad45402b683c68
ade1256 <adeprasetyo174@gmail.com>
2019-04-16 22:48:34 +0700

    Day 3 : Login Screen App

---
+++ day3_login_screen/README.md
@@ -0,0 +1,12 @@
+# Day 3 : Login Screen
+
+Day 3 : Sign in and sign up screen
+
+##### Demo
+
+![Alt text](demo.png?raw=true "Login Screen")
+
+##### References
+[Stackoverflow](https://stackoverflow.com/questions/50293503/how-to-set-the-width-of-a-raisedbutton-in-flutter)
+[Add Assets Image](https://flutter.dev/docs/development/ui/assets-and-images)
+[Ahmen Al-Zahrani](https://medium.com/flutterpub/a-simple-login-page-in-flutter-c2c65bb57072)
---
+++ day3_login_screen/lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginPageState();
+}
+
+// Used for controlling whether the user is loggin or creating an account
+enum FormType {
+  login,
+  register
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final TextEditingController _emailFilter = TextEditingController();
+  final TextEditingController _passwordFilter = TextEditingController();
+  String _email = "";
+  String _password = "";
+  FormType _form = FormType.login; // our default setting is to login, and we should switch to creating an account when the user chooses to
+
+  _LoginPageState() {
+    _emailFilter.addListener(_emailListen);
+    _passwordFilter.addListener(_passwordListen);
+  }
+
+  void _emailListen() {
+    if (_emailFilter.text.isEmpty) {
+      _email = "";
+    } else {
+      _email = _emailFilter.text;
+    }
+  }
+
+  void _passwordListen() {
+    if (_passwordFilter.text.isEmpty) {
+      _password = "";
+    } else {
+      _password = _passwordFilter.text;
+    }
+  }
+
+  // Swap in between our two forms, registering and logging in
+  void _formChange () async {
+    setState(() {
+      if (_form == FormType.register) {
+        _form = FormType.login;
+      } else {
+        _form = FormType.register;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildBar(context),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            _buildTextFields(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return AppBar(
+      title: Text("Sign in / Sign up"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildTextFields() {
+    return Container(
+     padding: EdgeInsets.only(top: 50.0,bottom: 50.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Image.asset('assets/smurf.png'),
+          ),
+          Container(
+            child: TextField(
+              controller: _emailFilter,
+              decoration: InputDecoration(
+                labelText: 'Email'
+              ),
+            ),
+          ),
+          Container(
+            child: TextField(
+              controller: _passwordFilter,
+              decoration: InputDecoration(
+                labelText: 'Password'
+              ),
+              obscureText: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    if (_form == FormType.login) {
+      return Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              width: 600.0,
+              child: RaisedButton(
+                child: Text('Login'),
+                onPressed: _loginPressed,
+              ),
+            ),
+            FlatButton(
+              child: Text('Dont have an account? Tap here to register.'),
+              onPressed: _formChange,
+            ),
+            FlatButton(
+              child: Text('Forgot Password?'),
+              onPressed: _passwordReset,
+            )
+          ],
+        ),
+      );
+    } else {
+      return Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Create an Account'),
+              onPressed: _createAccountPressed,
+            ),
+            FlatButton(
+              child: Text('Have an account? Click here to login.'),
+              onPressed: _formChange,
+            )
+          ],
+        ),
+      );
+    }
+  }
+
+  // These functions can self contain any user auth logic required, they all have access to _email and _password
+
+  void _loginPressed () {
+    print('The user wants to login with $_email and $_password');
+  }
+
+  void _createAccountPressed () {
+    print('The user wants to create an accoutn with $_email and $_password');
+
+  }
+
+  void _passwordReset () {
+    print("The user wants a password reset request sent to $_email");
+  }
+
+
+}
